ab2988b6f974f1d9acb2fb91e877cb2b140a4c52
Nguyen Huy <huy.nguyen8@sotatek.com>
2022-10-18 10:38:53 +0700

    init project

---
+++ src/main/java/com/cardano/explorer/ExplorerApiApplication.java
@@ -0,0 +1,13 @@
+package com.cardano.explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorerApiApplication.class, args);
+	}
+
+}
