5db59fe2f1d3651443113e7eb8a863f9f295056e
Nguyen Huy <huy.nguyen8@sotatek.com>
2022-10-18 10:38:53 +0700

    init project

---
+++ src/test/java/com/cardano/explorer/ExplorerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardano.explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
