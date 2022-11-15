a4ec2b68652f3c08277039a5333d83a03f497db9
Sotatek-SonVu3 <son.vu3@sotatek.com>
2022-11-15 17:46:07 +0700

    Config cross-origin resource sharing for all

--- src/main/java/com/cardano/explorer/config/WebConfig.java
+++ src/main/java/com/cardano/explorer/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.cardano.explorer.interceptor.AuthInterceptor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     log.info("Authentication interceptor is adding to the service");
     registry.addInterceptor(authInterceptor).addPathPatterns("/api/**");
+  }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD");
   }
 }
