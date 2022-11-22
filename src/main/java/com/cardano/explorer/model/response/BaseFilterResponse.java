93f0739b554a94859d7a5efb10d547cc7dde528f
Sotatek-PhucNguyen5 <phuc.nguyen5@sotatek.com>
2022-11-22 15:43:43 +0700

    format request vs response pool list

--- src/main/java/com/cardano/explorer/model/response/BaseFilterResponse.java
+++ src/main/java/com/cardano/explorer/model/response/BaseFilterResponse.java
@@ -3,6 +3,7 @@ package com.cardano.explorer.model.response;
 import java.io.Serializable;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class BaseFilterResponse<T> implements Serializable {
 
   private List<T> data;
