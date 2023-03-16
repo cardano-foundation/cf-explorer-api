c3981ea572575a2ef0064ac97ddcff4a455e9191
Sotatek-SonVu3 <son.vu3@sotatek.com>
2023-03-16 14:23:16 +0700

    feat: create api for get market data from coingecko

---
+++ src/main/java/com/cardano/explorer/controller/MarketDataController.java
@@ -0,0 +1,26 @@
+package com.cardano.explorer.controller;
+
+import com.cardano.explorer.config.LogMessage;
+import com.cardano.explorer.service.MarketDataService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/markets")
+@RequiredArgsConstructor
+public class MarketDataController {
+
+  private final MarketDataService marketDataService;
+
+  @GetMapping
+  @LogMessage
+  @Operation(summary = "Get ada cardano price, market cap, volume, and market related data")
+  public ResponseEntity<Object> getMarketData(@RequestParam("currency") String currency) {
+    return ResponseEntity.ok(marketDataService.getMarketData(currency));
+  }
+}
