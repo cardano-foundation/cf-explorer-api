6b9f5baa16bbc859a9ee881c7b77602cc9aee31b
Sotatek-SonVu3 <son.vu3@sotatek.com>
2023-03-20 21:42:09 +0700

    fix: MET-357 Do not display data of Policy script

--- src/main/java/com/cardano/explorer/repository/ScriptRepository.java
+++ src/main/java/com/cardano/explorer/repository/ScriptRepository.java
@@ -3,11 +3,9 @@ package com.cardano.explorer.repository;
 import com.sotatek.cardano.common.entity.Script;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ScriptRepository extends JpaRepository<Script, Long> {
 
-  @Query("SELECT script.json FROM Script script WHERE script.hash = :hash")
-  Optional<String> findJsonByHash(String hash);
+  Optional<Script> findByHash(String hash);
 
 }
