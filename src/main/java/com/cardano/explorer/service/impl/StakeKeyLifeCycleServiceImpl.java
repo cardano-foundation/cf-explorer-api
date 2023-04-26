80733c8a32769ccfebdc7e26312629211d86da7e
Sotatek-SonVu3 <son.vu3@sotatek.com>
2023-04-26 15:35:50 +0700

    feat: MET-723 delegation detail for stake key life cycle

--- src/main/java/com/cardano/explorer/service/impl/StakeKeyLifeCycleServiceImpl.java
+++ src/main/java/com/cardano/explorer/service/impl/StakeKeyLifeCycleServiceImpl.java
@@ -4,6 +4,7 @@ import com.cardano.explorer.exception.BusinessCode;
 import com.cardano.explorer.model.request.stake.StakeLifeCycleFilterRequest;
 import com.cardano.explorer.model.response.BaseFilterResponse;
 import com.cardano.explorer.model.response.stake.lifecycle.StakeRegistrationLifeCycle;
+import com.cardano.explorer.model.response.stake.lifecycle.lifecycle.StakeDelegationDetailResponse;
 import com.cardano.explorer.model.response.stake.lifecycle.lifecycle.StakeDelegationFilterResponse;
 import com.cardano.explorer.projection.StakeHistoryProjection;
 import com.cardano.explorer.repository.AddressTxBalanceRepository;
@@ -12,8 +13,12 @@ import com.cardano.explorer.repository.StakeAddressRepository;
 import com.cardano.explorer.repository.StakeDeRegistrationRepository;
 import com.cardano.explorer.repository.StakeRegistrationRepository;
 import com.cardano.explorer.service.StakeKeyLifeCycleService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.sotatek.cardano.common.entity.StakeAddress;
 import com.sotatek.cardanocommonapi.exceptions.BusinessException;
+import com.sotatek.cardanocommonapi.utils.StringUtils;
+import java.math.BigInteger;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -116,5 +121,34 @@ public class StakeKeyLifeCycleServiceImpl implements StakeKeyLifeCycleService {
                 .build()
         )
     );
+  }
+
+  @Override
+  public StakeDelegationDetailResponse getStakeDelegationDetail(String stakeKey, String hash) {
+    StakeAddress stakeAddress = stakeAddressRepository.findByView(stakeKey).orElseThrow(
+        () -> new BusinessException(BusinessCode.STAKE_ADDRESS_NOT_FOUND));
+    var delegation = delegationRepository.findDelegationByAddressAndTx(stakeAddress, hash)
+        .orElseThrow(() -> new BusinessException(BusinessCode.STAKE_DELEGATION_NOT_FOUND));
+    var totalBalance = addressTxBalanceRepository.getBalanceByStakeAddressAndTime(stakeAddress,
+        delegation.getTime()).orElse(BigInteger.ZERO);
+    return StakeDelegationDetailResponse.builder()
+        .fee(delegation.getFee())
+        .outSum(delegation.getOutSum())
+        .poolId(delegation.getPoolId())
+        .poolName(getNameValueFromJson(delegation.getPoolData()))
+        .time(delegation.getTime().toLocalDateTime())
+        .txHash(delegation.getTxHash())
+        .blockNo(delegation.getBlockNo())
+        .epoch(delegation.getEpochNo())
+        .stakeTotalAmount(totalBalance)
+        .build();
+  }
+
+  private String getNameValueFromJson(String json) {
+    if (Boolean.TRUE.equals(StringUtils.isNullOrEmpty(json))) {
+      return null;
+    }
+    JsonObject jsonObject = new Gson().fromJson(json, JsonObject.class);
+    return jsonObject.get("name").getAsString();
   }
 }
