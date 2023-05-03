ae77cc0b03a0b57983424f67af9ae5f4b8111f8d
Sotatek-SonVu3 <son.vu3@sotatek.com>
2023-05-03 14:08:18 +0700

    feat: MET-305 remove unused annotation

--- src/main/java/com/cardano/explorer/common/enumeration/StakeRewardType.java
+++ src/main/java/com/cardano/explorer/common/enumeration/StakeRewardType.java
@@ -1,13 +1,5 @@
 package com.cardano.explorer.common.enumeration;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@RequiredArgsConstructor
-@Getter
 public enum StakeRewardType {
   REWARD_RECEIVED,
   REWARD_WITHDRAWN
