4600eb511a115ab737df65740396833459845d01
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2023-05-11 09:41:07 +0100

    feat: added Standalone and Cluster mode for Redis

---
+++ src/main/java/org/cardanofoundation/explorer/api/config/redis/cluster/RedisClusterConfig.java
@@ -0,0 +1,58 @@
+package org.cardanofoundation.explorer.api.config.redis.cluster;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+
+import java.util.List;
+
+@Log4j2
+@Configuration
+@Profile("cluster")
+public class RedisClusterConfig {
+
+    @Value("${spring.redis.cluster.nodes}")
+    private List<String> nodes;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Bean(name = "lettuceConnectionFactory")
+    @Autowired
+    LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(nodes);
+        redisClusterConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisClusterConfiguration);
+    }
+
+    @Bean
+    @Autowired
+    RedisTemplate<String, ?> redisTemplate(//NOSONAR
+                                           final LettuceConnectionFactory lettuceConnectionFactory) {
+        var redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    @Autowired
+    RedisTemplate<String, String> redisTemplateString(//NOSONAR // TODO will remove in next version
+                                                      final LettuceConnectionFactory lettuceConnectionFactory) {
+        var redisTemplate = new RedisTemplate<String, String>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        return redisTemplate;
+    }
+}
