95ce0e28482a396ce0f680fcf29256fcb2a4633e
Sotatek-SonVu3 <son.vu3@sotatek.com>
2023-05-22 17:19:00 +0700

    fix: MET-400 optimize query top

--- src/main/java/org/cardanofoundation/explorer/api/repository/StakeAddressRepository.java
+++ src/main/java/org/cardanofoundation/explorer/api/repository/StakeAddressRepository.java
@@ -2,6 +2,8 @@ package org.cardanofoundation.explorer.api.repository;
 
 import org.cardanofoundation.explorer.api.projection.StakeAddressProjection;
 import org.cardanofoundation.explorer.consumercommon.entity.StakeAddress;
+
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
@@ -12,9 +14,9 @@ import org.springframework.data.repository.query.Param;
 
 public interface StakeAddressRepository extends JpaRepository<StakeAddress, Long> {
 
-  Optional<StakeAddress> findByView(@Param("view") String view);
+  Optional<StakeAddress> findByView(@Param("aLong") String aLong);
 
-  @Query(value = "SELECT sa.view as stakeAddress, sum(addr.balance) as totalStake"
+  @Query(value = "SELECT sa.id as id, sa.view as stakeAddress, sum(addr.balance) as totalStake"
       + " FROM StakeAddress sa"
       + " LEFT JOIN Address addr ON addr.stakeAddress = sa"
       + " WHERE EXISTS (SELECT d FROM Delegation d WHERE d.address = sa)"
@@ -22,6 +24,16 @@ public interface StakeAddressRepository extends JpaRepository<StakeAddress, Long
       + " HAVING sum(addr.balance) IS NOT NULL"
       + " ORDER BY totalStake DESC")
   List<StakeAddressProjection> findStakeAddressOrderByBalance(Pageable pageable);
+
+  @Query(value = "SELECT sa.view as stakeAddress, sum(addr.balance) as totalStake"
+      + " FROM StakeAddress sa"
+      + " LEFT JOIN Address addr ON addr.stakeAddress = sa"
+      + " WHERE sa.id in (:stakeKeyIds)"
+      + " GROUP BY sa.id"
+      + " HAVING sum(addr.balance) IS NOT NULL"
+      + " ORDER BY totalStake DESC")
+  List<StakeAddressProjection> findStakeAddressOrderByBalance(@Param("stakeKeyIds") Collection<Long> stakeKeyIds,
+                                                              Pageable pageable);
 
   @Query(value = "SELECT ph.view FROM StakeAddress sa "
       + "JOIN PoolOwner po ON sa.id  = po.stakeAddress.id "
