f30999cf5da58c1a3eeaba1bdf930128993aff81
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-05-31 17:25:25 +0700

    feat: met-777 remove unused logic

--- src/main/java/org/cardanofoundation/explorer/api/model/response/pool/report/PoolReportDetailResponse.java
+++ src/main/java/org/cardanofoundation/explorer/api/model/response/pool/report/PoolReportDetailResponse.java
@@ -7,16 +7,10 @@ import org.cardanofoundation.explorer.api.model.response.pool.lifecycle.RewardRe
 import org.cardanofoundation.explorer.api.model.response.pool.lifecycle.TabularRegisResponse;
 import org.cardanofoundation.explorer.api.model.response.pool.projection.PoolReportProjection;
 import org.cardanofoundation.explorer.api.util.DataUtil;
-import org.cardanofoundation.explorer.api.util.report.ColumnFieldEnum;
-import org.cardanofoundation.explorer.api.util.report.ColumnTitleEnum;
-import org.cardanofoundation.explorer.api.util.report.ExportColumn;
-import org.cardanofoundation.explorer.api.util.report.ExportColumn.Alignment;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -58,22 +52,6 @@ public class PoolReportDetailResponse {
           .size(new BigDecimal(projection.getSize()))
           .build();
     }
-
-    public static List<ExportColumn> designFile(Boolean isFeePaid) {
-      List<ExportColumn> epochSizeColumns = new ArrayList<>();
-      epochSizeColumns.add(
-          new ExportColumn(ColumnFieldEnum.SIZE_COLUMN, ColumnTitleEnum.SIZE_TITLE,
-                           Alignment.RIGHT));
-      if (Boolean.TRUE.equals(isFeePaid)) {
-        epochSizeColumns.add(
-            new ExportColumn(ColumnFieldEnum.FEE_COLUMN, ColumnTitleEnum.FEES_TITLE,
-                             Alignment.RIGHT));
-      }
-      epochSizeColumns.add(
-          new ExportColumn(ColumnFieldEnum.EPOCH_COLUMN, ColumnTitleEnum.EPOCH_TITLE,
-                           Alignment.RIGHT));
-      return epochSizeColumns;
-    }
   }
 
   @Data
@@ -105,23 +83,6 @@ public class PoolReportDetailResponse {
       result.setAdaValue(new BigDecimal(response.getTotalFee()));
       return result;
     }
-
-    public static List<ExportColumn> designFile() {
-      List<ExportColumn> poolRegistrationsColumns = new ArrayList<>();
-      poolRegistrationsColumns.add(
-          new ExportColumn(ColumnFieldEnum.TX_HASH_COLUMN, ColumnTitleEnum.TX_HASH_TITLE,
-                           Alignment.LEFT, 61 * 255));
-      poolRegistrationsColumns.add(
-          new ExportColumn(ColumnFieldEnum.TIME_COLUMN, ColumnTitleEnum.TIMESTAMP_TITLE,
-                           Alignment.CENTER));
-      poolRegistrationsColumns.add(
-          new ExportColumn(ColumnFieldEnum.ADA_VALUE_COLUMN, ColumnTitleEnum.ADA_VALUE_TITLE,
-                           Alignment.RIGHT));
-      poolRegistrationsColumns.add(
-          new ExportColumn(ColumnFieldEnum.OWNER_COLUMN, ColumnTitleEnum.OWNER_TITLE,
-                           Alignment.LEFT, 59 * 255));
-      return poolRegistrationsColumns;
-    }
   }
 
   @Data
@@ -151,22 +112,6 @@ public class PoolReportDetailResponse {
       return result;
     }
 
-    public static List<ExportColumn> designFile() {
-      List<ExportColumn> poolUpdateColumns = new ArrayList<>();
-      poolUpdateColumns.add(
-          new ExportColumn(ColumnFieldEnum.TX_HASH_COLUMN, ColumnTitleEnum.TX_HASH_TITLE,
-                           Alignment.LEFT, 61 * 255));
-      poolUpdateColumns.add(
-          new ExportColumn(ColumnFieldEnum.TIME_COLUMN, ColumnTitleEnum.TIMESTAMP_TITLE,
-                           Alignment.CENTER));
-      poolUpdateColumns.add(
-          new ExportColumn(ColumnFieldEnum.ADA_VALUE_COLUMN, ColumnTitleEnum.ADA_VALUE_TITLE,
-                           Alignment.RIGHT));
-      poolUpdateColumns.add(new ExportColumn(ColumnFieldEnum.ADA_VALUE_FEE_COLUMN,
-                                             ColumnTitleEnum.ADA_VALUE_FEE_TITLE,
-                                             Alignment.RIGHT));
-      return poolUpdateColumns;
-    }
   }
 
   @Data
@@ -192,22 +137,6 @@ public class PoolReportDetailResponse {
           .build();
     }
 
-    public static List<ExportColumn> designFile() {
-      List<ExportColumn> rewardDistributionColumns = new ArrayList<>();
-      rewardDistributionColumns.add(
-          new ExportColumn(ColumnFieldEnum.EPOCH_COLUMN, ColumnTitleEnum.EPOCH_TITLE,
-                           Alignment.RIGHT));
-      rewardDistributionColumns.add(
-          new ExportColumn(ColumnFieldEnum.TIME_COLUMN, ColumnTitleEnum.TIMESTAMP_TITLE,
-                           Alignment.CENTER));
-      rewardDistributionColumns.add(new ExportColumn(ColumnFieldEnum.OPERATOR_REWARD_COLUMN,
-                                                     ColumnTitleEnum.OPERATOR_REWARD_TITLE,
-                                                     Alignment.RIGHT));
-      rewardDistributionColumns.add(new ExportColumn(ColumnFieldEnum.REWARD_ACCOUNT_COLUMN,
-                                                     ColumnTitleEnum.REWARD_ACCOUNT_TITLE,
-                                                     Alignment.LEFT, 59 * 255));
-      return rewardDistributionColumns;
-    }
   }
 
   @Data
@@ -242,25 +171,6 @@ public class PoolReportDetailResponse {
       return result;
     }
 
-    public static List<ExportColumn> designFile() {
-      List<ExportColumn> deregistrationColumns = new ArrayList<>();
-      deregistrationColumns.add(
-          new ExportColumn(ColumnFieldEnum.TX_HASH_COLUMN, ColumnTitleEnum.TX_HASH_TITLE,
-                           Alignment.LEFT, 61 * 255));
-      deregistrationColumns.add(
-          new ExportColumn(ColumnFieldEnum.TIME_COLUMN, ColumnTitleEnum.DATE_TITLE,
-                           Alignment.CENTER));
-      deregistrationColumns.add(
-          new ExportColumn(ColumnFieldEnum.ADA_VALUE_COLUMN, ColumnTitleEnum.ADA_VALUE_TITLE,
-                           Alignment.RIGHT));
-      deregistrationColumns.add(new ExportColumn(ColumnFieldEnum.ADA_VALUE_FEE_COLUMN,
-                                                 ColumnTitleEnum.ADA_VALUE_FEE_TITLE,
-                                                 Alignment.RIGHT));
-      deregistrationColumns.add(
-          new ExportColumn(ColumnFieldEnum.OWNER_COLUMN, ColumnTitleEnum.OWNER_TITLE,
-                           Alignment.LEFT, 59 * 255));
-      return deregistrationColumns;
-    }
   }
 
 }
--- src/main/java/org/cardanofoundation/explorer/api/service/impl/PoolReportServiceImpl.java
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/PoolReportServiceImpl.java
@@ -20,8 +20,6 @@ import org.cardanofoundation.explorer.api.service.PoolLifecycleService;
 import org.cardanofoundation.explorer.api.service.PoolReportService;
 import org.cardanofoundation.explorer.api.service.StorageService;
 import org.cardanofoundation.explorer.api.util.DataUtil;
-import org.cardanofoundation.explorer.api.util.report.ExcelHelper;
-import org.cardanofoundation.explorer.api.util.report.ExportContent;
 import org.cardanofoundation.explorer.common.exceptions.BusinessException;
 import org.cardanofoundation.explorer.consumercommon.entity.PoolReportHistory;
 import org.cardanofoundation.explorer.consumercommon.entity.ReportHistory;
@@ -29,13 +27,11 @@ import org.cardanofoundation.explorer.consumercommon.enumeration.ReportStatus;
 import org.cardanofoundation.explorer.consumercommon.enumeration.ReportType;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -43,9 +39,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -53,11 +47,6 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Log4j2
 public class PoolReportServiceImpl implements PoolReportService {
-  private static final String POOL_SIZE_TITLE = "Pool Size";
-  private static final String POOL_REGISTRATIONS_TITLE = "Pool Registrations";
-  private static final String POOL_UPDATE_TITLE = "Pool Update";
-  private static final String REWARD_DISTRIBUTION_TITLE = "Reward Distribution";
-  private static final String DEREGISTRATION_TITLE = "Deregistration";
 
   private final PoolReportRepository poolReportRepository;
 
@@ -69,8 +58,6 @@ public class PoolReportServiceImpl implements PoolReportService {
 
   private final PoolHashRepository poolHashRepository;
   private final KafkaService kafkaService;
-
-  private final Pageable defaultPageable = PageRequest.of(0, 1000, Sort.by("id").descending());
 
   @Override
   public Boolean create(PoolReportCreateRequest poolReportCreateRequest, String username) {
@@ -108,88 +95,6 @@ public class PoolReportServiceImpl implements PoolReportService {
           .byteArrayInputStream(new ByteArrayInputStream(bytes))
           .build();
     }
-  }
-
-  @Transactional
-  public void exportDirect(PoolReportHistory poolReport) {
-    try {
-      List<ExportContent> exportContents = getExportContents(poolReport);
-      String storageKey = generateStorageKey(poolReport);
-      String excelFileName = storageKey + ExportType.EXCEL.getValue();
-      InputStream excelInputStream = ExcelHelper.writeContent(exportContents);
-      storageService.uploadFile(excelInputStream.readAllBytes(), excelFileName);
-      poolReport.getReportHistory().setStatus(ReportStatus.GENERATED);
-      poolReport.getReportHistory().setStorageKey(storageKey);
-      poolReportRepository.save(poolReport);
-      excelInputStream.close();
-    } catch (Exception e) {
-      poolReport.getReportHistory().setStatus(ReportStatus.FAILED);
-      log.error("Error while generating report", e);
-    } finally {
-      poolReportRepository.save(poolReport);
-    }
-  }
-
-  private List<ExportContent> getExportContents(PoolReportHistory poolReport) {
-    List<ExportContent> exportContents = new ArrayList<>();
-    /// pool registrations
-    if (Boolean.TRUE.equals(poolReport.getEventRegistration())) {
-      var poolRegistrationBaseFilterResponse = fetchPoolRegistration(poolReport);
-
-      exportContents.add(ExportContent.builder()
-                             .clazz(PoolReportDetailResponse.PoolRegistration.class)
-                             .headerTitle(POOL_REGISTRATIONS_TITLE)
-                             .lstColumn(PoolReportDetailResponse.PoolRegistration.designFile())
-                             .lstData(poolRegistrationBaseFilterResponse.getData())
-                             .build());
-    }
-    // pool update
-    if (Boolean.TRUE.equals(poolReport.getEventPoolUpdate())) {
-      var poolUpdateBaseFilterResponse = fetchPoolUpdate(poolReport);
-
-      exportContents.add(ExportContent.builder()
-                             .clazz(PoolReportDetailResponse.PoolUpdate.class)
-                             .headerTitle(POOL_UPDATE_TITLE)
-                             .lstColumn(PoolReportDetailResponse.PoolUpdate.designFile())
-                             .lstData(poolUpdateBaseFilterResponse.getData())
-                             .build());
-    }
-    // reward distribution
-    if (Boolean.TRUE.equals(poolReport.getEventReward())) {
-      var rewardDistributionBaseFilterResponse = fetchRewardsDistribution(poolReport);
-
-      exportContents.add(ExportContent.builder()
-                             .clazz(PoolReportDetailResponse.RewardDistribution.class)
-                             .headerTitle(REWARD_DISTRIBUTION_TITLE)
-                             .lstColumn(PoolReportDetailResponse.RewardDistribution.designFile())
-                             .lstData(rewardDistributionBaseFilterResponse.getData())
-                             .build());
-    }
-    // deregistration
-    if (Boolean.TRUE.equals(poolReport.getEventDeregistration())) {
-      var deregistrationBaseFilterResponse = fetchDeregistraion(poolReport);
-
-      exportContents.add(ExportContent.builder()
-                             .clazz(PoolReportDetailResponse.Deregistration.class)
-                             .headerTitle(DEREGISTRATION_TITLE)
-                             .lstColumn(PoolReportDetailResponse.Deregistration.designFile())
-                             .lstData(deregistrationBaseFilterResponse.getData())
-                             .build());
-    }
-    /// epoch size
-    if (Boolean.TRUE.equals(poolReport.getIsPoolSize())) {
-      var epochSizeBaseFilterResponse = fetchEpochSize(poolReport);
-
-      exportContents.add(ExportContent.builder()
-                             .clazz(PoolReportDetailResponse.EpochSize.class)
-                             .headerTitle(POOL_SIZE_TITLE)
-                             .lstColumn(PoolReportDetailResponse.EpochSize.designFile(
-                                 poolReport.getIsFeesPaid()))
-                             .lstData(epochSizeBaseFilterResponse.getData())
-                             .build());
-    }
-
-    return exportContents;
   }
 
   @Override
@@ -267,59 +172,6 @@ public class PoolReportServiceImpl implements PoolReportService {
                                                                             reportId);
     return poolLifecycleService.deRegistration(poolReport.getPoolView(), null, null, null,
                                                pageable);
-  }
-
-  private BaseFilterResponse<PoolReportDetailResponse.EpochSize> fetchEpochSize(
-      PoolReportHistory poolReport) {
-    List<PoolReportProjection> epochSizeProjections = epochStakeRepository.getEpochSizeByPoolReport(
-        poolReport.getPoolView(), poolReport.getBeginEpoch(), poolReport.getEndEpoch());
-    List<PoolReportDetailResponse.EpochSize> epochSizes = epochSizeProjections.stream()
-        .map(PoolReportDetailResponse.EpochSize::toDomain).collect(Collectors.toList());
-    return new BaseFilterResponse<>(epochSizes, epochSizeProjections.size());
-  }
-
-  private BaseFilterResponse<PoolReportDetailResponse.PoolRegistration> fetchPoolRegistration(
-      PoolReportHistory poolReport) {
-
-    List<TabularRegisResponse> tabularRegisResponses = poolLifecycleService.registrationList(
-        poolReport.getPoolView(), defaultPageable).getData();
-    List<PoolReportDetailResponse.PoolRegistration> poolRegistrations = tabularRegisResponses.stream()
-        .map(PoolReportDetailResponse.PoolRegistration::toDomain).collect(Collectors.toList());
-    return new BaseFilterResponse<>(poolRegistrations, poolRegistrations.size());
-  }
-
-  private BaseFilterResponse<PoolReportDetailResponse.PoolUpdate> fetchPoolUpdate(
-      PoolReportHistory poolReport) {
-
-    List<PoolUpdateDetailResponse> poolUpdateDetailResponses = poolLifecycleService.poolUpdateList(
-        poolReport.getPoolView(), defaultPageable).getData();
-    List<PoolReportDetailResponse.PoolUpdate> poolUpdates = poolUpdateDetailResponses.stream()
-        .map(PoolReportDetailResponse.PoolUpdate::toDomain).collect(Collectors.toList());
-    return new BaseFilterResponse<>(poolUpdates, poolUpdates.size());
-  }
-
-  private BaseFilterResponse<PoolReportDetailResponse.RewardDistribution> fetchRewardsDistribution(
-      PoolReportHistory poolReport) {
-
-    List<RewardResponse> rewardResponses = poolLifecycleService.listReward(
-        poolReport.getPoolView(), defaultPageable).getData();
-    List<PoolReportDetailResponse.RewardDistribution> rewardDistributions = rewardResponses.stream()
-        .map(PoolReportDetailResponse.RewardDistribution::toDomain).collect(Collectors.toList());
-    return new BaseFilterResponse<>(rewardDistributions, rewardDistributions.size());
-  }
-
-  private BaseFilterResponse<PoolReportDetailResponse.Deregistration> fetchDeregistraion(
-      PoolReportHistory poolReport) {
-    List<DeRegistrationResponse> deRegistrationResponses = poolLifecycleService.deRegistration(
-        poolReport.getPoolView(), null, null, null, defaultPageable).getData();
-    List<PoolReportDetailResponse.Deregistration> deregistrations = deRegistrationResponses.stream()
-        .map(PoolReportDetailResponse.Deregistration::toDomain).collect(Collectors.toList());
-    return new BaseFilterResponse<>(deregistrations, deregistrations.size());
-  }
-
-  private String generateStorageKey(PoolReportHistory poolReport) {
-    return poolReport.getReportHistory().getId() + "_" + poolReport.getReportHistory()
-        .getReportName();
   }
 
   private ReportHistory initReportHistory(PoolReportCreateRequest poolReportCreateRequest,
