5117d45232c58992705870d289ce83dc173f3d88
Sotatek-TungNguyen <tung.nguyen6@sotatek.com>
2023-06-05 19:25:30 +0700

    get entity from common
    get entity from common

--- src/main/java/org/cardanofoundation/explorer/api/repository/AggregateAddressTxBalanceRepository.java
+++ src/main/java/org/cardanofoundation/explorer/api/repository/AggregateAddressTxBalanceRepository.java
@@ -1,6 +1,6 @@
 package org.cardanofoundation.explorer.api.repository;
 
-import org.cardanofoundation.explorer.api.common.AggregateAddressTxBalance;
+import org.cardanofoundation.explorer.consumercommon.entity.aggregation.AggregateAddressTxBalance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
