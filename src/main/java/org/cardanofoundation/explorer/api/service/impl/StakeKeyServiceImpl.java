72013d9c99cfc4a6fcdaa0f75299bcdb4f3787f6
Sotatek-PhucNguyen5 <phuc.nguyen5@sotatek.com>
2023-06-09 10:40:05 +0700

    fix: fetch realtime pool info from koios

--- src/main/java/org/cardanofoundation/explorer/api/service/impl/StakeKeyServiceImpl.java
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/StakeKeyServiceImpl.java
@@ -1,5 +1,7 @@
 package org.cardanofoundation.explorer.api.service.impl;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.math.BigInteger;
@@ -292,18 +294,9 @@ public class StakeKeyServiceImpl implements StakeKeyService {
     BigInteger activeStake = null;
     BigInteger liveStake = null;
     if (Boolean.TRUE.equals(isKoiOs)) {
-      Set<String> poolViews = poolHashRepository.findAllSetPoolView();
-      Boolean isInfo = fetchRewardDataService.checkPoolInfoForPool(poolViews);
-      if (Boolean.FALSE.equals(isInfo)) {
-        Boolean isFetch = fetchRewardDataService.fetchPoolInfoForPool(poolViews);
-        if (Boolean.TRUE.equals(isFetch)) {
-          activeStake = poolInfoRepository.getTotalActiveStake(currentEpoch);
-          liveStake = poolInfoRepository.getTotalLiveStake(currentEpoch);
-        }
-      } else {
-        activeStake = poolInfoRepository.getTotalActiveStake(currentEpoch);
-        liveStake = poolInfoRepository.getTotalLiveStake(currentEpoch);
-      }
+      handleFetchPoolInfo(poolHashRepository.findAllSetPoolView());
+      activeStake = poolInfoRepository.getTotalActiveStake(currentEpoch);
+      liveStake = poolInfoRepository.getTotalLiveStake(currentEpoch);
     } else {
       Object activeStakeObj = redisTemplate.opsForValue()
           .get(CommonConstant.REDIS_TOTAL_ACTIVATE_STAKE + network + "_" + currentEpoch);
@@ -424,4 +417,34 @@ public class StakeKeyServiceImpl implements StakeKeyService {
     return jsonObject.get("name").getAsString();
   }
 
+  private CompletableFuture<Boolean> fetchPoolInfoKoiOs(Set<String> poolIds) {
+    return CompletableFuture.supplyAsync(
+        () -> fetchRewardDataService.fetchPoolInfoForPool(poolIds));
+  }
+
+  private void handleFetchPoolInfo(Set<String> poolIds) {
+    List<CompletableFuture<Boolean>> completableFutures = new ArrayList<>();
+    List<List<String>> subPoolIdList = Lists.partition(Lists.newArrayList(poolIds), 100);
+    subPoolIdList.forEach(
+        poolIdList -> completableFutures.add(fetchPoolInfoKoiOs(Sets.newHashSet(poolIdList))));
+    CompletableFuture<Void> combinedFuture
+        = CompletableFuture.allOf(
+        completableFutures.toArray(new CompletableFuture[0]));
+    CompletableFuture<List<Boolean>> allResultFuture = combinedFuture.thenApply(v ->
+        completableFutures.stream().map(CompletableFuture::join)
+            .toList()
+    );
+    CompletableFuture<Boolean> resultFetch = allResultFuture.thenApply(results ->
+        results.stream().allMatch(result -> result.equals(Boolean.TRUE))
+    ).exceptionally(ex -> {
+      log.error("Error: when fetch data from koios");
+      return false;
+    });
+    try {
+      resultFetch.get();
+    } catch (InterruptedException | ExecutionException e) {
+      log.error("Error: " + e);
+      Thread.currentThread().interrupt();
+    }
+  }
 }
