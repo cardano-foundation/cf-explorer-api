f06622c713f3119d62a341ef21412307190af7eb
Sotatek-PhucNguyen5 <phuc.nguyen5@sotatek.com>
2023-06-16 09:52:40 +0700

    fix: add hold for pool lifecycle registration

--- src/main/java/org/cardanofoundation/explorer/api/repository/PoolUpdateRepository.java
+++ src/main/java/org/cardanofoundation/explorer/api/repository/PoolUpdateRepository.java
@@ -93,8 +93,10 @@ public interface PoolUpdateRepository extends JpaRepository<PoolUpdate, Long> {
           + "AND (CAST(:fromDate AS timestamp) IS NULL OR bk.time >= :fromDate) "
           + "AND (CAST(:toDate AS timestamp) IS NULL OR bk.time <= :toDate) ")
   Page<PoolUpdateProjection> findPoolUpdateByPool(@Param("poolView") String poolView,
-                                                  @Param("txHash") String txHash, @Param("fromDate") Timestamp fromDate,
-                                                  @Param("toDate") Timestamp toDate, Pageable pageable);
+      @Param("txHash") String txHash,
+      @Param("fromDate") Timestamp fromDate,
+      @Param("toDate") Timestamp toDate,
+      Pageable pageable);
 
 
   @Query(value =
@@ -143,7 +145,7 @@ public interface PoolUpdateRepository extends JpaRepository<PoolUpdate, Long> {
   List<String> findPoolByRewardAccount(@Param("stakeAddress") StakeAddress stakeAddress);
 
   @Query(value =
-      "SELECT pu.id AS poolUpdateId, tx.hash AS txHash, tx.fee AS fee, bk.time AS time, pu.margin AS margin "
+      "SELECT pu.id AS poolUpdateId, tx.hash AS txHash, tx.fee AS fee, bk.time AS time, pu.margin AS margin, tx.deposit AS deposit "
           + "FROM PoolHash ph "
           + "JOIN PoolUpdate pu ON ph.id = pu.poolHash.id "
           + "JOIN Tx tx ON pu.registeredTx.id  = tx.id AND tx.deposit IS NOT NULL AND tx.deposit > 0 "
