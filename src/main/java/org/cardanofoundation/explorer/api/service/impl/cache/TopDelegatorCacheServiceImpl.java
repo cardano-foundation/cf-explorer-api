3a655b35e4fc820f47fd593c04c992648fd27fc6
Sotatek-TungNguyen <tung.nguyen6@sotatek.com>
2023-06-16 11:44:45 +0700

    feat: [MET-1348] Move cron job top delegators to Schedule

--- src/main/java/org/cardanofoundation/explorer/api/service/impl/cache/TopDelegatorCacheServiceImpl.java
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/cache/TopDelegatorCacheServiceImpl.java
@@ -5,15 +5,9 @@ import com.google.gson.reflect.TypeToken;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.cardanofoundation.explorer.api.common.enumeration.RedisKey;
-import org.cardanofoundation.explorer.api.projection.StakeAddressProjection;
-import org.cardanofoundation.explorer.api.repository.StakeAddressRepository;
 import org.cardanofoundation.explorer.api.service.cache.TopDelegatorCacheService;
-import org.cardanofoundation.explorer.api.util.StreamUtil;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -25,29 +19,13 @@ import java.util.List;
 @Slf4j
 public class TopDelegatorCacheServiceImpl implements TopDelegatorCacheService {
 
-  private static final int MAX_ELEMENT_CACHE = 500;
-  private static final int FIVE_MINUTES_IN_MILLISECONDS = 5 * 60 * 1000;
   private static final String COLON = ":";
   private static final Gson gson = new Gson();
 
   private final RedisTemplate<String, Object> redisTemplate;
-  private final StakeAddressRepository stakeAddressRepository;
 
   @Value("${application.network}")
   String network;
-
-  //todo: move this to scheduler-service
-  @Scheduled(fixedDelay = FIVE_MINUTES_IN_MILLISECONDS)
-  public void buildTopStakeDelegatorCache() {
-    String redisKey = RedisKey.REDIS_TOP_STAKE_DELEGATORS.name() + COLON + network;
-    long start = System.currentTimeMillis();
-    Pageable pageable = PageRequest.of(0, MAX_ELEMENT_CACHE);
-    var stakeAddressProjections = stakeAddressRepository.findStakeAddressOrderByBalance(pageable);
-    var stakeIds = StreamUtil.mapApply(stakeAddressProjections, StakeAddressProjection::getId);
-
-    redisTemplate.opsForValue().set(redisKey, gson.toJson(stakeIds));
-    log.info("Build top-stake-delegators cache successfully, takes: [{} ms]", (System.currentTimeMillis() - start));
-  }
 
   public List<Long> getTopStakeDelegatorCache() {
     String redisKey = RedisKey.REDIS_TOP_STAKE_DELEGATORS.name() + COLON + network;
