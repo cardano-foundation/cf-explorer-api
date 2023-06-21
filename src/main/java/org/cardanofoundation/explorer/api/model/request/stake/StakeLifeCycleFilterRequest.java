a358924e66b1805543478a671499a62d679dc653
ryan2nguyen <an.nguyen4@sotatek.com>
2023-06-21 15:19:07 +0700

    chore: add validate annotation

--- src/main/java/org/cardanofoundation/explorer/api/model/request/stake/StakeLifeCycleFilterRequest.java
+++ src/main/java/org/cardanofoundation/explorer/api/model/request/stake/StakeLifeCycleFilterRequest.java
@@ -4,14 +4,20 @@ import java.util.Date;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.cardanofoundation.explorer.api.common.constant.CommonConstant;
+import org.cardanofoundation.explorer.common.validation.date.DatePattern;
+import org.cardanofoundation.explorer.common.validation.date.param.DateValid;
+import org.cardanofoundation.explorer.common.validation.length.LengthValid;
 
 @Getter
 @Setter
 @EqualsAndHashCode
 public class StakeLifeCycleFilterRequest {
 
+  @LengthValid(CommonConstant.TX_HASH_LENGTH)
   private String txHash;
+  @DateValid(pattern = DatePattern.YYYY_MM_DD)
   private Date fromDate;
+  @DateValid(pattern = DatePattern.YYYY_MM_DD)
   private Date toDate;
-
 }
--- src/main/java/org/cardanofoundation/explorer/api/model/request/stake/report/ReportHistoryFilterRequest.java
+++ src/main/java/org/cardanofoundation/explorer/api/model/request/stake/report/ReportHistoryFilterRequest.java
@@ -4,12 +4,16 @@ import java.util.Date;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.cardanofoundation.explorer.common.validation.date.DatePattern;
+import org.cardanofoundation.explorer.common.validation.date.param.DateValid;
 
 @Getter
 @Setter
 @Builder
 public class ReportHistoryFilterRequest {
+  @DateValid(pattern = DatePattern.YYYY_MM_DD)
   private Date fromDate;
+  @DateValid(pattern = DatePattern.YYYY_MM_DD)
   private Date toDate;
   private String reportName;
 }
