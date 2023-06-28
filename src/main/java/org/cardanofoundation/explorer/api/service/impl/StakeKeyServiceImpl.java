da1d14655329bf0ac56c0b1cde8212b2592b371e
Sotatek-TungNguyen <tung.nguyen6@sotatek.com>
2023-06-28 20:29:52 +0700

    feat: MET-1143-return-zero when-no-transaction

--- src/main/java/org/cardanofoundation/explorer/api/service/impl/StakeKeyServiceImpl.java
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/StakeKeyServiceImpl.java
@@ -337,14 +337,6 @@ public class StakeKeyServiceImpl implements StakeKeyService {
       balance = getBalanceInRangePreviousToday(stakeAddress, to, maxDateAgg.get());
     }
 
-    if (BigInteger.ZERO.equals(balance)) {
-      Long numberBalanceRecord = addressTxBalanceRepository.countRecord(
-          stakeAddress, Timestamp.valueOf(to.atTime(LocalTime.MAX))
-      );
-      boolean isNoRecord = numberBalanceRecord == null || numberBalanceRecord ==  0;
-      balance = isNoRecord ? null : balance;
-    }
-
     return new StakeAnalyticBalanceResponse(to, balance);
   }
 
