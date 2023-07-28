226a58ab090aa2e5770a99f5adb95f2d94438d41
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-07-29 01:01:23 +0700

    fix: add logo endpoint in front of relative path logo

--- src/main/java/org/cardanofoundation/explorer/api/mapper/AssetMetadataMapper.java
+++ src/main/java/org/cardanofoundation/explorer/api/mapper/AssetMetadataMapper.java
@@ -1,10 +1,26 @@
 package org.cardanofoundation.explorer.api.mapper;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import org.cardanofoundation.explorer.api.model.response.token.TokenMetadataResponse;
 import org.cardanofoundation.explorer.consumercommon.entity.AssetMetadata;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
-public interface AssetMetadataMapper {
+public abstract class AssetMetadataMapper {
 
-  TokenMetadataResponse fromAssetMetadata(AssetMetadata metadata);
+  @Value("${application.token-logo-endpoint}")
+  protected String tokenLogoEndpoint;
+
+  @Mapping(target = "logo", source = "logo", qualifiedByName = "getTokenLogoURL")
+  public abstract TokenMetadataResponse fromAssetMetadata(AssetMetadata metadata);
+
+  @Named("getTokenLogoURL")
+  String getTokenLogoEndpoint(String logo){
+    return Objects.isNull(logo) ? null : (tokenLogoEndpoint + logo);
+  }
 }
