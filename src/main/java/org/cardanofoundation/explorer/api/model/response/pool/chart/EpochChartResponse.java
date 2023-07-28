2032ba10d71cab5bbad5c3ba5eec1e91db9875d0
Sotatek-PhucNguyen5 <phuc.nguyen5@sotatek.com>
2023-07-28 16:46:18 +0700

    fix: set default value for pool chart api

--- src/main/java/org/cardanofoundation/explorer/api/model/response/pool/chart/EpochChartResponse.java
+++ src/main/java/org/cardanofoundation/explorer/api/model/response/pool/chart/EpochChartResponse.java
@@ -1,7 +1,13 @@
 package org.cardanofoundation.explorer.api.model.response.pool.chart;
 
 import java.math.BigInteger;
+import java.util.List;
 
 public class EpochChartResponse extends BasePoolChart<EpochChartList, BigInteger> {
 
+  public EpochChartResponse() {
+    this.setDataByDays(List.of());
+    this.setLowest(BigInteger.ZERO);
+    this.setHighest(BigInteger.ZERO);
+  }
 }
