c4b820c15bc8084d56a49177a88eb5a5aaea42a8
Sotatek-SonVu3 <son.vu3@sotatek.com>
2023-08-09 18:04:06 +0700

    fix: MET-1563 change query to check payment address and stake address

--- src/main/java/org/cardanofoundation/explorer/api/model/response/token/TokenAddressResponse.java
+++ src/main/java/org/cardanofoundation/explorer/api/model/response/token/TokenAddressResponse.java
@@ -14,7 +14,6 @@ import lombok.*;
 @JsonInclude(Include.NON_NULL)
 public class TokenAddressResponse {
   private Long addressId;
-  private Long numberOfPaymentAddress;
   private String address;
   private String stakeAddress;
   private String policy;
