32ce49676614f9f518b4e3958b713e6b87394dd9
sotatek-huyle3 <huy.le3@sotatek.com>
2023-08-29 09:55:32 +0700

    feat: [met-1514] implement sync-healthcheck endpoint

---
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/HealthCheckServiceImpl.java
@@ -0,0 +1,55 @@
+package org.cardanofoundation.explorer.api.service.impl;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import org.cardanofoundation.explorer.api.model.response.healthcheck.SyncStatus;
+import org.cardanofoundation.explorer.api.repository.BlockRepository;
+import org.cardanofoundation.explorer.api.service.HealthCheckService;
+import org.cardanofoundation.explorer.api.service.cache.AggregatedDataCacheService;
+import org.cardanofoundation.explorer.consumercommon.entity.Block;
+
+@Service
+@RequiredArgsConstructor
+public class HealthCheckServiceImpl implements HealthCheckService {
+
+  private final AggregatedDataCacheService aggregatedDataCacheService;
+  private final BlockRepository blockRepository;
+
+  @Value("${application.healthcheck.block-time-threshold}")
+  private String blockTimeThreshold;
+
+  @Override
+  public SyncStatus getSyncStatus() {
+    SyncStatus syncStatus = new SyncStatus();
+
+    LocalDateTime latestBlockTime = aggregatedDataCacheService.getLatestBlockTime();
+    LocalDateTime latestBlockInsertTime = aggregatedDataCacheService.getLatestBlockInsertTime();
+
+    if (Objects.isNull(latestBlockTime)) {
+      Optional<Block> latestBlock = blockRepository.findLatestBlock();
+      if (latestBlock.isEmpty()) {
+        syncStatus.setIsSyncing(Boolean.FALSE);
+        return syncStatus;
+      }
+      latestBlockTime = latestBlock.get().getTime().toLocalDateTime();
+    }
+
+    long secondsSinceLatestBlock = ChronoUnit.SECONDS.between(latestBlockTime,
+        LocalDateTime.now(ZoneOffset.UTC));
+    long thresholdBlockTimeInSeconds = Long.parseLong(blockTimeThreshold);
+
+    Boolean isSyncing = secondsSinceLatestBlock <= thresholdBlockTimeInSeconds;
+
+    return SyncStatus.builder().isSyncing(isSyncing).latestBlockInsertTime(latestBlockInsertTime)
+        .build();
+  }
+}
---
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/cache/RedisAggregatedDateCacheServiceImpl.java
@@ -0,0 +1,63 @@
+package org.cardanofoundation.explorer.api.service.impl.cache;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import org.cardanofoundation.explorer.api.common.constant.CommonConstant;
+import org.cardanofoundation.explorer.api.service.cache.AggregatedDataCacheService;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RedisAggregatedDateCacheServiceImpl implements AggregatedDataCacheService {
+
+  private static final String AGGREGATED_CACHE_KEY = "AGGREGATED_CACHE";
+  private static final String BLOCK_TIME_HASH_KEY = "LATEST_BLOCK_TIME";
+  private static final String BLOCK_INSERT_TIME_HASH_KEY = "LATEST_BLOCK_INSERT_TIME";
+  private final RedisTemplate<String, String> redisTemplate;
+
+  private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(
+      "yyyy-MM-dd HH:mm:ss");
+
+  @Value("${application.network}")
+  private String network;
+
+  @Override
+  public LocalDateTime getLatestBlockTime() {
+    String blockTime = getRedisHashValue(BLOCK_TIME_HASH_KEY);
+    if (Objects.isNull(blockTime)) {
+      return null;
+    }
+    return LocalDateTime.parse(blockTime, DATE_TIME_FORMATTER);
+  }
+
+  @Override
+  public LocalDateTime getLatestBlockInsertTime() {
+    String blockTime = getRedisHashValue(BLOCK_INSERT_TIME_HASH_KEY);
+    if (Objects.isNull(blockTime)) {
+      return null;
+    }
+    return LocalDateTime.parse(blockTime, DATE_TIME_FORMATTER);
+  }
+
+  private String getRedisHashValue(String hashKey) {
+    HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
+
+    String redisKey = getRedisKey(AGGREGATED_CACHE_KEY);
+    return hashOperations.get(redisKey, hashKey);
+  }
+
+  private String getRedisKey(String key) {
+    return String.join(CommonConstant.UNDERSCORE, network.toUpperCase(), key);
+  }
+
+}
