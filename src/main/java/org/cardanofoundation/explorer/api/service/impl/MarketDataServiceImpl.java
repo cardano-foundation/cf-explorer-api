1687ac55e97c70c402c3fadb75d42b6e4d772acc
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-08-31 15:30:43 +0700

    feat: publish market price to websocket

--- src/main/java/org/cardanofoundation/explorer/api/service/impl/MarketDataServiceImpl.java
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/MarketDataServiceImpl.java
@@ -1,9 +1,15 @@
 package org.cardanofoundation.explorer.api.service.impl;
 
-import org.cardanofoundation.explorer.api.service.MarketDataService;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.cardanofoundation.explorer.api.common.enumeration.WebSocketEventType;
+import org.cardanofoundation.explorer.api.event.websocket.WebSocketEvent;
+import org.cardanofoundation.explorer.api.event.websocket.WebSocketMessage;
+import org.cardanofoundation.explorer.api.service.MarketDataService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,8 +22,35 @@ public class MarketDataServiceImpl implements MarketDataService {
 
   private final RestTemplate restTemplate;
 
-  @Cacheable(value = "market", key = "#currency", cacheManager = "cacheManager")
+  private final ApplicationEventPublisher applicationEventPublisher;
+
+  private final RedisTemplate<String, Object> redisTemplate;
+
   public Object getMarketData(String currency) {
-    return restTemplate.getForObject(String.format(apiMarketDataUrl, currency), Object.class);
+    Object cachedData = redisTemplate.opsForValue().get(currency);
+    if (cachedData == null) {
+      cachedData =
+          restTemplate.getForObject(String.format(apiMarketDataUrl, currency), Object.class);
+      redisTemplate.opsForValue().set(currency, cachedData, Duration.ofSeconds(30));
+    }
+    return cachedData;
+  }
+
+  @Scheduled(fixedDelay = 4000)
+  public void publishMarketData() {
+    WebSocketMessage messagePriceUsd =
+        WebSocketMessage.builder()
+            .eventType(WebSocketEventType.CURRENT_PRICE_USD)
+            .payload(getMarketData("usd"))
+            .build();
+
+    WebSocketMessage messagePriceBtc =
+        WebSocketMessage.builder()
+            .eventType(WebSocketEventType.CURRENT_PRICE_BTC)
+            .payload(getMarketData("btc"))
+            .build();
+
+    applicationEventPublisher.publishEvent(new WebSocketEvent(messagePriceUsd) {});
+    applicationEventPublisher.publishEvent(new WebSocketEvent(messagePriceBtc) {});
   }
 }
