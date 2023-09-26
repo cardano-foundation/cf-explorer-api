a573656862da3f758857fb7ff147981c651dcdc7
Sotatek-HaiPham2 <hai.pham2@sotatek.com>
2023-09-26 13:54:53 +0700

    feat: update cache fixed protocol

--- src/main/java/org/cardanofoundation/explorer/api/model/response/protocol/HistoriesProtocol.java
+++ src/main/java/org/cardanofoundation/explorer/api/model/response/protocol/HistoriesProtocol.java
@@ -17,35 +17,35 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @NoArgsConstructor
 @JsonInclude(value = Include.NON_EMPTY)
 public class HistoriesProtocol {
-   List<EpochChange> epochChanges;
-   List<ProtocolHistory> minFeeA;
-   List<ProtocolHistory> minFeeB;
-   List<ProtocolHistory> maxBlockSize;
-   List<ProtocolHistory> maxTxSize;
-   List<ProtocolHistory> maxBhSize;
-   List<ProtocolHistory> keyDeposit;
-   List<ProtocolHistory> poolDeposit;
-   List<ProtocolHistory> maxEpoch;
-   List<ProtocolHistory> optimalPoolCount;
-   List<ProtocolHistory> influence;
-   List<ProtocolHistory> monetaryExpandRate;
-   List<ProtocolHistory> treasuryGrowthRate;
-   List<ProtocolHistory> decentralisation;
-   List<ProtocolHistory> entropy;
-   List<ProtocolHistory> protocolMajor;
-   List<ProtocolHistory> protocolMinor;
-   List<ProtocolHistory> minUtxoValue;
-   List<ProtocolHistory> minPoolCost;
-   List<ProtocolHistory> costModel;
-   List<ProtocolHistory> priceMem;
-   List<ProtocolHistory> priceStep;
-   List<ProtocolHistory> maxTxExMem;
-   List<ProtocolHistory> maxTxExSteps;
-   List<ProtocolHistory> maxBlockExMem;
-   List<ProtocolHistory> maxBlockExSteps;
-   List<ProtocolHistory> maxValSize;
-   List<ProtocolHistory> collateralPercent;
-   List<ProtocolHistory> maxCollateralInputs;
-   @JsonProperty("coinsPerUtxoByte")
-   List<ProtocolHistory> coinsPerUtxoSize;
+  List<EpochChange> epochChanges;
+  List<ProtocolHistory> minFeeA;
+  List<ProtocolHistory> minFeeB;
+  List<ProtocolHistory> maxBlockSize;
+  List<ProtocolHistory> maxTxSize;
+  List<ProtocolHistory> maxBhSize;
+  List<ProtocolHistory> keyDeposit;
+  List<ProtocolHistory> poolDeposit;
+  List<ProtocolHistory> maxEpoch;
+  List<ProtocolHistory> optimalPoolCount;
+  List<ProtocolHistory> influence;
+  List<ProtocolHistory> monetaryExpandRate;
+  List<ProtocolHistory> treasuryGrowthRate;
+  List<ProtocolHistory> decentralisation;
+  List<ProtocolHistory> entropy;
+  List<ProtocolHistory> protocolMajor;
+  List<ProtocolHistory> protocolMinor;
+  List<ProtocolHistory> minUtxoValue;
+  List<ProtocolHistory> minPoolCost;
+  List<ProtocolHistory> costModel;
+  List<ProtocolHistory> priceMem;
+  List<ProtocolHistory> priceStep;
+  List<ProtocolHistory> maxTxExMem;
+  List<ProtocolHistory> maxTxExSteps;
+  List<ProtocolHistory> maxBlockExMem;
+  List<ProtocolHistory> maxBlockExSteps;
+  List<ProtocolHistory> maxValSize;
+  List<ProtocolHistory> collateralPercent;
+  List<ProtocolHistory> maxCollateralInputs;
+  @JsonProperty("coinsPerUTxOByte")
+  List<ProtocolHistory> coinsPerUtxoSize;
 }
