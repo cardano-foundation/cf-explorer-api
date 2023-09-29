25b9354c9da9d7a5e67481875668542dc4852711
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-09-29 10:22:05 +0700

    fix: update logic to publish market data to ws

--- src/main/java/org/cardanofoundation/explorer/api/service/impl/MarketDataServiceImpl.java
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/MarketDataServiceImpl.java
@@ -1,8 +1,13 @@
 package org.cardanofoundation.explorer.api.service.impl;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import lombok.RequiredArgsConstructor;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.cardanofoundation.explorer.api.common.enumeration.WebSocketEventType;
 import org.cardanofoundation.explorer.api.event.websocket.WebSocketEvent;
 import org.cardanofoundation.explorer.api.event.websocket.WebSocketMessage;
@@ -26,8 +31,14 @@ public class MarketDataServiceImpl implements MarketDataService {
   private final ApplicationEventPublisher applicationEventPublisher;
 
   private final RedisTemplate<String, Object> redisTemplate;
+
+  private final ObjectMapper objectMapper;
+  private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
   private static final String REDIS_PREFIX_KEY = "MARKET_DATA";
   private static final String UNDERSCORE = "_";
+  private static final String LAST_UPDATED_FIELD = "last_updated";
+  private static final String CURRENT_PRICE_FIELD = "current_price";
+  
 
   public Object getMarketData(String currency) {
     String redisKey = String.join(UNDERSCORE, REDIS_PREFIX_KEY, currency.toUpperCase());
@@ -45,24 +56,43 @@ public class MarketDataServiceImpl implements MarketDataService {
     Object marketDataCachedObject = redisTemplate.opsForValue().get(redisKey);
     Object marketDataObject =
         restTemplate.getForObject(String.format(apiMarketDataUrl, currency), Object.class);
-    LinkedHashMap<String, Object> marketData =
-        (LinkedHashMap<String, Object>) ((ArrayList) marketDataObject).get(0);
-    LinkedHashMap<String, Object> marketDataCached =
-        (LinkedHashMap<String, Object>) ((ArrayList) marketDataCachedObject).get(0);
-    if (!marketData
-        .get("last_updated")
-        .toString()
-        .equals(marketDataCached.get("last_updated").toString())) {
-      WebSocketMessage messageCurrentPrice =
-          WebSocketMessage.builder().payload(marketDataObject).build();
+    JsonNode marketDataNode = objectMapper.valueToTree(marketDataObject);
+    JsonNode marketDataCachedNode = objectMapper.valueToTree(marketDataCachedObject);
+
+    // if market data is null, do return
+    if (marketDataObject == null) {
+      return;
+    }
+
+    // if marketDataCachedNode is null and last_updated field is not equal, do update cache value
+    if (marketDataCachedNode == null ||
+        (!compareMarketDataField(marketDataNode, marketDataCachedNode, LAST_UPDATED_FIELD) &&
+            !compareMarketDataField(marketDataNode, marketDataCachedNode, CURRENT_PRICE_FIELD))) {
+      redisTemplate.opsForValue().set(redisKey, marketDataObject);
+      // overwrite last_updated field in marketDataNode to current time
+      ((ObjectNode) marketDataNode.get(0)).put(LAST_UPDATED_FIELD, dateFormat.format(new Date()));
+      WebSocketMessage messageCurrentPrice = WebSocketMessage.builder()
+          .payload(objectMapper.convertValue(marketDataNode, Object.class))
+          .build();
+
       if (currency.equals("usd")) {
         messageCurrentPrice.setEventType(WebSocketEventType.CURRENT_PRICE_USD);
       } else if (currency.equals("btc")) {
         messageCurrentPrice.setEventType(WebSocketEventType.CURRENT_PRICE_BTC);
       }
-      redisTemplate.opsForValue().set(redisKey, marketDataObject);
       applicationEventPublisher.publishEvent(new WebSocketEvent(messageCurrentPrice) {});
     }
+  }
+
+  /**
+   * Compare market data field
+   * @param marketDataNode
+   * @param marketDataCachedNode
+   * @param field
+   * @return true if value of field is equal, otherwise false
+   */
+  private boolean compareMarketDataField(JsonNode marketDataNode, JsonNode marketDataCachedNode, String field) {
+    return marketDataNode.get(0).get(field).asText().equals(marketDataCachedNode.get(0).get(field).asText());
   }
 
   @Scheduled(fixedDelayString = "${application.api.coin.gecko.market.delay-time}")
