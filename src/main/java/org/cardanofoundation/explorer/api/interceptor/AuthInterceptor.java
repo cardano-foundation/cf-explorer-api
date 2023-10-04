372e7da2a9f140dc9a61c594c5e6f191d8a2f7eb
Sotatek-HaiPham2 <hai.pham2@sotatek.com>
2023-10-04 17:46:49 +0700

    feat: use interceptor

--- src/main/java/org/cardanofoundation/explorer/api/interceptor/AuthInterceptor.java
+++ src/main/java/org/cardanofoundation/explorer/api/interceptor/AuthInterceptor.java
@@ -1,10 +1,15 @@
 package org.cardanofoundation.explorer.api.interceptor;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import lombok.RequiredArgsConstructor;
@@ -14,15 +19,23 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.cardanofoundation.explorer.api.common.constant.CommonConstant;
 import org.cardanofoundation.explorer.api.config.RsaConfig;
+import org.cardanofoundation.explorer.api.security.auth.RoleConfigurationMapper;
+import org.cardanofoundation.explorer.api.security.auth.RoleFilterMapper;
+import org.cardanofoundation.explorer.api.security.auth.RoleFunction;
+import org.cardanofoundation.explorer.api.security.auth.UserPrincipal;
 import org.cardanofoundation.explorer.common.annotation.IgnoreAuthentication;
 import org.cardanofoundation.explorer.common.exceptions.BusinessException;
 import org.cardanofoundation.explorer.common.exceptions.InvalidAccessTokenException;
 import org.cardanofoundation.explorer.common.exceptions.enums.CommonErrorCode;
 import org.cardanofoundation.explorer.common.utils.JwtUtils;
 import org.cardanofoundation.explorer.common.utils.StringUtils;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
@@ -30,9 +43,95 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Log4j2
 public class AuthInterceptor implements HandlerInterceptor {
 
+  private final RoleFilterMapper roleConf;
+
   private final RsaConfig rsaConfig;
 
   private final RedisTemplate<String, Object> redisTemplate;
+
+  private List<AntPathRequestMatcher> matchers;
+
+  @PostConstruct
+  public void initAuth() {
+    matchers = roleConf.getAuth().stream()
+        .map(request -> {
+          if (org.springframework.util.StringUtils.hasText(request.getMethod())
+              || request.getMethod().equals("*")) {
+            return new AntPathRequestMatcher(request.getUri());
+          }
+          return new AntPathRequestMatcher(request.getUri(), request.getMethod());
+        }).toList();
+  }
+
+
+  @Override
+  public boolean preHandle(
+      HttpServletRequest request, HttpServletResponse httpServletResponse,
+      Object handler)
+      throws Exception {
+    log.info("Authentication Interceptor is running...");
+    HandlerMethod handlerMethod;
+    try {
+      handlerMethod = (HandlerMethod) handler;
+    } catch (ClassCastException e) {
+      log.error("Error handler: " + e.getMessage());
+      return HandlerInterceptor.super.preHandle(request, httpServletResponse, handler);
+    }
+    Method method = handlerMethod.getMethod();
+    log.info("Authentication Interceptor: {}", method);
+    if (method.isAnnotationPresent(IgnoreAuthentication.class)) {
+      log.info("Ignore method if marked IgnoreAuthentication annotation");
+      return true;
+    }
+    if (method.getDeclaringClass().isAnnotationPresent(IgnoreAuthentication.class)) {
+      log.info("Ignore class if marked IgnoreAuthentication annotation");
+      return true;
+    }
+
+    if (matchers.stream().noneMatch(matcher -> matcher.matches(request))) {
+      return true;
+    }
+    UserPrincipal userPrincipal = new UserPrincipal();
+    String token = getToken(request, userPrincipal);
+    Set<String> roles = getRoles(token);
+    Map<String, Map<String, Object>> roleDescription = new HashMap<>();
+    for (RoleConfigurationMapper roleMapper : roleConf.getRoles()) {
+      if (roles.contains(roleMapper.getName())) {
+        String roleKey = roleMapper.getName();
+        Map<String, Object> desc = roleMapper.getFunction().stream()
+            .filter(Objects::nonNull)
+            .findAny().orElse(new RoleFunction()).getDescription();
+
+        if (Objects.nonNull(desc)) {
+          roleDescription.put(roleKey, desc);
+        }
+      }
+    }
+    userPrincipal.setRoleDescription(roleDescription);
+    request.setAttribute("user", userPrincipal);
+    return true;
+
+  }
+
+
+  private String getToken(HttpServletRequest request, UserPrincipal userPrincipal) {
+    String token = JwtUtils.parseJwt(request);
+    try {
+      JwtUtils.validateJwtToken(token, rsaConfig.getPublicKey());
+    } catch (Exception e) {
+      throw new InvalidAccessTokenException();
+    }
+    if (isTokenBlacklisted(token)) {
+      throw new InvalidAccessTokenException();
+    }
+    String username = JwtUtils.getAccountIdFromJwtToken(token, rsaConfig.getPublicKey());
+    userPrincipal.setUsername(username);
+    return token;
+  }
+
+  private Set<String> getRoles(String token) {
+    return JwtUtils.getRolesFromToken(token, rsaConfig.getPublicKey());
+  }
 
   private boolean isTokenBlacklisted(String token) {
     if (Boolean.TRUE.equals(StringUtils.isNullOrEmpty(token))) {
@@ -40,37 +139,4 @@ public class AuthInterceptor implements HandlerInterceptor {
     }
     return redisTemplate.opsForValue().get(CommonConstant.JWT + token) != null;
   }
-
-//  @Override
-//  public boolean preHandle(
-//      HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
-//      Object handler)
-//      throws Exception {
-//    log.info("Authentication Interceptor is running...");
-//    HandlerMethod handlerMethod;
-//    try {
-//      handlerMethod = (HandlerMethod) handler;
-//    } catch (ClassCastException e) {
-//      log.error("Error handler: " + e.getMessage());
-//      return HandlerInterceptor.super.preHandle(httpServletRequest, httpServletResponse, handler);
-//    }
-//    Method method = handlerMethod.getMethod();
-//    log.info("Authentication Interceptor: {}", method);
-//    if (method.isAnnotationPresent(IgnoreAuthentication.class)) {
-//      log.info("Ignore method if marked IgnoreAuthentication annotation");
-//      return true;
-//    }
-//    if (method.getDeclaringClass().isAnnotationPresent(IgnoreAuthentication.class)) {
-//      log.info("Ignore class if marked IgnoreAuthentication annotation");
-//      return true;
-//    }
-//    String token = JwtUtils.parseJwt(httpServletRequest);
-//    JwtUtils.validateJwtToken(token, rsaConfig.getPublicKey());
-//    if (isTokenBlacklisted(token)) {
-//      throw new InvalidAccessTokenException();
-//    }
-//    String username = JwtUtils.getAccountIdFromJwtToken(token, rsaConfig.getPublicKey());
-//    httpServletRequest.setAttribute("username", username);
-//    return true;
-//  }
 }
--- src/main/java/org/cardanofoundation/explorer/api/security/configuration/SpringWebSecurityConfig.java
+++ src/main/java/org/cardanofoundation/explorer/api/security/configuration/SpringWebSecurityConfig.java
@@ -1,42 +1,19 @@
 package org.cardanofoundation.explorer.api.security.configuration;
 
-import java.util.List;
-import java.util.Set;
-
 import lombok.AllArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import org.cardanofoundation.explorer.api.config.RsaConfig;
-import org.cardanofoundation.explorer.api.controller.advice.GlobalRestControllerExceptionHandler;
-import org.cardanofoundation.explorer.api.security.auth.Request;
-import org.cardanofoundation.explorer.api.security.auth.RoleFilterMapper;
-import org.cardanofoundation.explorer.api.security.filter.DynamicFilter;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class SpringWebSecurityConfig {
 
-  private RoleFilterMapper roleConf;
-
-  private RsaConfig rsaConfig;
-
-  private RedisTemplate<String, Object> redisTemplate;
-
-
-  @Qualifier("handlerExceptionResolver")
-  private HandlerExceptionResolver handlerExceptionResolver;
 
   @Bean
   public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
@@ -53,9 +30,6 @@ public class SpringWebSecurityConfig {
                         (policy) ->
                             policy.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN))
                     .permissionsPolicy((policy) -> policy.policy("geolocation=(self)")));
-    http.addFilterAfter(
-        new DynamicFilter(roleConf, rsaConfig, redisTemplate, handlerExceptionResolver),
-        BasicAuthenticationFilter.class);
     return http.build();
   }
 
