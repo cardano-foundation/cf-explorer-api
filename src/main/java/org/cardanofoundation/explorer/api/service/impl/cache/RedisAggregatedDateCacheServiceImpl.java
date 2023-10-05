f43a2b2b486cc451d68188ed491c1337473c5907
sotatek-huyle3 <huy.le3@sotatek.com>
2023-10-05 17:20:20 +0700

    feat: MET-1700 use aggregated data for token detail and token list endpoint

--- src/main/java/org/cardanofoundation/explorer/api/service/impl/cache/RedisAggregatedDateCacheServiceImpl.java
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/cache/RedisAggregatedDateCacheServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import org.cardanofoundation.explorer.api.common.constant.CommonConstant;
 import org.cardanofoundation.explorer.api.service.cache.AggregatedDataCacheService;
@@ -22,6 +23,7 @@ public class RedisAggregatedDateCacheServiceImpl implements AggregatedDataCacheS
 
   private static final String AGGREGATED_CACHE_KEY = "AGGREGATED_CACHE";
   private static final String BLOCK_TIME_HASH_KEY = "LATEST_BLOCK_TIME";
+  private static final String TOKEN_COUNT_HASH_KEY = "TOTAL_TOKEN_COUNT";
   private static final String BLOCK_INSERT_TIME_HASH_KEY = "LATEST_BLOCK_INSERT_TIME";
   private final RedisTemplate<String, String> redisTemplate;
 
@@ -47,6 +49,12 @@ public class RedisAggregatedDateCacheServiceImpl implements AggregatedDataCacheS
       return null;
     }
     return LocalDateTime.parse(blockTime, DATE_TIME_FORMATTER);
+  }
+
+  @Override
+  public int getTokenCount() {
+    String redisValue = getRedisHashValue(TOKEN_COUNT_HASH_KEY);
+    return StringUtils.hasText(redisValue) ? Integer.parseInt(redisValue) : 0;
   }
 
   private String getRedisHashValue(String hashKey) {
