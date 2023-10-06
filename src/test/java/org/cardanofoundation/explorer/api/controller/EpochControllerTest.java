599171e2730d6ea96c331937ae9240b5c608517d
Sotatek-HaiPham2 <hai.pham2@sotatek.com>
2023-10-06 08:53:10 +0700

    chore: update unit test

--- src/test/java/org/cardanofoundation/explorer/api/controller/EpochControllerTest.java
+++ src/test/java/org/cardanofoundation/explorer/api/controller/EpochControllerTest.java
@@ -6,6 +6,7 @@ import org.cardanofoundation.explorer.api.config.SpringWebSecurityConfig;
 import org.cardanofoundation.explorer.api.config.WebConfig;
 import org.cardanofoundation.explorer.api.controller.advice.GlobalRestControllerExceptionHandler;
 import org.cardanofoundation.explorer.api.interceptor.AuthInterceptor;
+import org.cardanofoundation.explorer.api.interceptor.auth.RoleFilterMapper;
 import org.cardanofoundation.explorer.api.model.response.*;
 import org.cardanofoundation.explorer.api.model.response.dashboard.EpochSummary;
 import org.cardanofoundation.explorer.api.service.BlockService;
@@ -36,7 +37,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         SpringWebSecurityConfig.class,
         WebConfig.class,
         JacksonMapperDateConfig.class,
-        GlobalRestControllerExceptionHandler.class
+        GlobalRestControllerExceptionHandler.class,
+        RoleFilterMapper.class
 })
 @AutoConfigureMockMvc(addFilters = false)
 public class EpochControllerTest {
