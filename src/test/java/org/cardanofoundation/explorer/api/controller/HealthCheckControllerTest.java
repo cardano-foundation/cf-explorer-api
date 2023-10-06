8c580bdcfa82da2547da618c999bd1b2dbe1a451
Sotatek-HaiPham2 <hai.pham2@sotatek.com>
2023-10-06 08:53:10 +0700

    chore: update unit test

--- src/test/java/org/cardanofoundation/explorer/api/controller/HealthCheckControllerTest.java
+++ src/test/java/org/cardanofoundation/explorer/api/controller/HealthCheckControllerTest.java
@@ -16,6 +16,7 @@ import org.cardanofoundation.explorer.api.config.SpringWebSecurityConfig;
 import org.cardanofoundation.explorer.api.config.WebConfig;
 import org.cardanofoundation.explorer.api.controller.advice.GlobalRestControllerExceptionHandler;
 import org.cardanofoundation.explorer.api.interceptor.AuthInterceptor;
+import org.cardanofoundation.explorer.api.interceptor.auth.RoleFilterMapper;
 import org.cardanofoundation.explorer.api.model.response.healthcheck.SyncStatus;
 import org.cardanofoundation.explorer.api.service.HealthCheckService;
 
@@ -33,7 +34,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
     SpringWebSecurityConfig.class,
     WebConfig.class,
     JacksonMapperDateConfig.class,
-    GlobalRestControllerExceptionHandler.class
+    GlobalRestControllerExceptionHandler.class,
+    RoleFilterMapper.class
 })
 @AutoConfigureMockMvc(addFilters = false)
 class HealthCheckControllerTest {
