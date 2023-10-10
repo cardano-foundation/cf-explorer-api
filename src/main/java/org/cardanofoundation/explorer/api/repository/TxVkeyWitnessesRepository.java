9d3fca41839da13ff8081a8050ea1f1f4c8a82e4
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-10-10 15:35:23 +0700

    feat: MET-1705 add signers information for MIR tx

---
+++ src/main/java/org/cardanofoundation/explorer/api/repository/TxVkeyWitnessesRepository.java
@@ -0,0 +1,14 @@
+package org.cardanofoundation.explorer.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import org.cardanofoundation.explorer.consumercommon.entity.Tx;
+import org.cardanofoundation.explorer.consumercommon.entity.TxVkeyWitness;
+
+public interface TxVkeyWitnessesRepository extends JpaRepository<TxVkeyWitness, Long> {
+
+  List<TxVkeyWitness> findAllByTx(@Param("tx") Tx tx);
+}
