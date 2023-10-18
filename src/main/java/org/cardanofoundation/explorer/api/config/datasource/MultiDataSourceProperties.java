c88ba77affb3d63f50a985fa56e6aac3551a5873
Sotatek-DucPhung <duc.phung@sotatek.com>
2023-10-18 10:48:33 +0700

    feat: MET-1722 multi datasource config (api)

---
+++ src/main/java/org/cardanofoundation/explorer/api/config/datasource/MultiDataSourceProperties.java
@@ -0,0 +1,59 @@
+package org.cardanofoundation.explorer.api.config.datasource;
+
+import java.util.Objects;
+
+import javax.sql.DataSource;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.flywaydb.core.Flyway;
+
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Configuration
+@ConfigurationProperties(prefix = "multi-datasource")
+public class MultiDataSourceProperties {
+
+  DataSourceConfig datasourceLedgerSync;
+  DataSourceConfig datasourceAnalytics;
+
+  @Data
+  @FieldDefaults(level = AccessLevel.PRIVATE)
+  public static class DataSourceConfig {
+    HikariConfig hikariConfig;
+    FlywayProperties flyway;
+  }
+
+  public DataSource buildDataSource(DataSourceConfig dataSourceConfig) {
+    HikariDataSource hikariDataSource = new HikariDataSource(dataSourceConfig.getHikariConfig());
+    FlywayProperties flywayProperties = dataSourceConfig.getFlyway();
+    if (flywayProperties != null && flywayProperties.isEnabled()) {
+      Flyway.configure()
+          .dataSource(hikariDataSource)
+          .locations(flywayProperties.getLocations().toArray(new String[0]))
+          .sqlMigrationPrefix(flywayProperties.getSqlMigrationPrefix())
+          .sqlMigrationSeparator(flywayProperties.getSqlMigrationSeparator())
+          .sqlMigrationSuffixes(flywayProperties.getSqlMigrationSuffixes().toArray(new String[0]))
+          .group(flywayProperties.isGroup())
+          .defaultSchema(flywayProperties.getDefaultSchema())
+          .schemas(flywayProperties.getSchemas().toArray(new String[0]))
+          .validateOnMigrate(flywayProperties.isValidateOnMigrate())
+          .outOfOrder(flywayProperties.isOutOfOrder())
+          .load()
+          .migrate();
+    }
+    return hikariDataSource;
+  }
+}
