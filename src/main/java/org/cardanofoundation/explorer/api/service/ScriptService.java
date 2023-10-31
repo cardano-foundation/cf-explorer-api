fcaf07bdf20e8fd6be51a6087e8d91f7f28e6fcb
Sotatek-SonVu3 <son.vu3@sotatek.com>
2023-10-31 09:58:29 +0700

    feat: MET-1742 create native script detail and related api

--- src/main/java/org/cardanofoundation/explorer/api/service/ScriptService.java
+++ src/main/java/org/cardanofoundation/explorer/api/service/ScriptService.java
@@ -1,13 +1,23 @@
 package org.cardanofoundation.explorer.api.service;
 
+import org.cardanofoundation.explorer.api.model.request.ScriptVerifyRequest;
 import org.cardanofoundation.explorer.api.model.response.BaseFilterResponse;
 import org.cardanofoundation.explorer.api.model.response.script.nativescript.NativeScriptFilterResponse;
+import org.cardanofoundation.explorer.api.model.response.script.nativescript.NativeScriptResponse;
 import org.cardanofoundation.explorer.api.model.response.script.smartcontract.SmartContractFilterResponse;
+import org.cardanofoundation.explorer.api.model.response.token.TokenAddressResponse;
+import org.cardanofoundation.explorer.api.model.response.token.TokenFilterResponse;
 import org.springframework.data.domain.Pageable;
 
 public interface ScriptService {
 
   BaseFilterResponse<NativeScriptFilterResponse> getNativeScripts(Pageable pageable);
+
+  NativeScriptResponse getNativeScripts(String scriptHash);
+
+  BaseFilterResponse<TokenFilterResponse> getNativeScriptTokens(String scriptHash, Pageable pageable);
+
+  BaseFilterResponse<TokenAddressResponse> getNativeScriptHolders(String scriptHash, Pageable pageable);
 
   BaseFilterResponse<SmartContractFilterResponse> getSmartContracts(Pageable pageable);
 }
