d949f6f33ebf5b138ca7996a83ddd4038135ea2f
Sotatek-SonVu3 <son.vu3@sotatek.com>
2023-10-31 09:58:29 +0700

    feat: MET-1742 create native script detail and related api

--- src/main/java/org/cardanofoundation/explorer/api/controller/ScriptController.java
+++ src/main/java/org/cardanofoundation/explorer/api/controller/ScriptController.java
@@ -1,11 +1,18 @@
 package org.cardanofoundation.explorer.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.cardanofoundation.explorer.api.config.LogMessage;
+import org.cardanofoundation.explorer.api.model.request.ScriptVerifyRequest;
 import org.cardanofoundation.explorer.api.model.response.BaseFilterResponse;
 import org.cardanofoundation.explorer.api.model.response.script.nativescript.NativeScriptFilterResponse;
+import org.cardanofoundation.explorer.api.model.response.script.nativescript.NativeScriptResponse;
 import org.cardanofoundation.explorer.api.model.response.script.smartcontract.SmartContractFilterResponse;
+import org.cardanofoundation.explorer.api.model.response.token.TokenAddressResponse;
+import org.cardanofoundation.explorer.api.model.response.token.TokenFilterResponse;
 import org.cardanofoundation.explorer.api.service.ScriptService;
 import org.cardanofoundation.explorer.common.validation.pagination.Pagination;
 import org.cardanofoundation.explorer.common.validation.pagination.PaginationDefault;
@@ -15,9 +22,7 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/scripts")
@@ -33,6 +38,30 @@ public class ScriptController {
       @ParameterObject @PaginationValid @PaginationDefault(size = 20, sort = {Script_.TX},
           direction = Sort.Direction.DESC) @Valid Pagination pagination) {
     return ResponseEntity.ok(scriptService.getNativeScripts(pagination.toPageable()));
+  }
+
+  @GetMapping("/native-scripts/{scriptHash}")
+  public ResponseEntity<NativeScriptResponse> getNativeScriptDetail(
+      @PathVariable String scriptHash) {
+    return ResponseEntity.ok(scriptService.getNativeScripts(scriptHash));
+  }
+
+  @GetMapping("/native-scripts/{scriptHash}/tokens")
+  @LogMessage
+  @Operation(summary = "Get tokens of a policy", description = "Get all tokens of a policy")
+  public ResponseEntity<BaseFilterResponse<TokenFilterResponse>> getTokens(
+      @PathVariable @Parameter(description = "The native script hash") String scriptHash,
+      @ParameterObject @PaginationValid @Valid Pagination pagination) {
+    return ResponseEntity.ok(scriptService.getNativeScriptTokens(scriptHash, pagination.toPageable()));
+  }
+
+  @GetMapping("/native-scripts/{scriptHash}/holders")
+  @LogMessage
+  @Operation(summary = "Get holders by policy", description = "Get all holders of all tokens of policy")
+  public ResponseEntity<BaseFilterResponse<TokenAddressResponse>> getHolders(
+      @PathVariable @Parameter(description = "The native script hash") String scriptHash,
+      @ParameterObject @PaginationValid @Valid Pagination pagination) {
+    return ResponseEntity.ok(scriptService.getNativeScriptHolders(scriptHash, pagination.toPageable()));
   }
 
   @GetMapping("/smart-contracts")
