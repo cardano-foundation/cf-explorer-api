286cb21bf689705fc3c22781fe07649a4841366a
Sotatek-PhucNguyen5 <phuc.nguyen5@sotatek.com>
2023-12-01 16:45:34 +0700

    feat: pass with music version v1 or v2

--- src/main/java/org/cardanofoundation/explorer/api/common/constant/CommonConstant.java
+++ src/main/java/org/cardanofoundation/explorer/api/common/constant/CommonConstant.java
@@ -92,7 +92,7 @@ public class CommonConstant {
   public static final String[] FIELD_TYPE = {"string", "string | raw bytes", "uri | array",
       "image/<mime_sub_type>", "string | array", "mime_type", "1 or 2", "uri", "array",
       "raw bytes", "Single / Multiple", "uint", "string: iso8601 duration format", "array: string",
-      "bool", "map: <string, string>"};
+      "bool", "map: <string, string>", "1 or 2,v1 or v2"};
 
 
   public static int hashCode(Object... a) {
