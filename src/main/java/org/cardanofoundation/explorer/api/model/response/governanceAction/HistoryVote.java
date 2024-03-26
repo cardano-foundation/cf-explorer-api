a0dd73865bcb864f681912b5a53c6ed3a6d3869c
Sotatek-ThinhVu <thinh.vu@sotatek.com>
2024-03-26 10:53:51 +0700

    feat: fix logic

--- src/main/java/org/cardanofoundation/explorer/api/model/response/governanceAction/HistoryVote.java
+++ src/main/java/org/cardanofoundation/explorer/api/model/response/governanceAction/HistoryVote.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import org.cardanofoundation.explorer.common.entity.ledgersync.enumeration.Vote;
 
 @Getter
@@ -21,4 +23,8 @@ public class HistoryVote {
   Vote vote;
 
   Date timestamp;
+
+  @JsonIgnore String txHash;
+
+  @JsonIgnore String txIndex;
 }
