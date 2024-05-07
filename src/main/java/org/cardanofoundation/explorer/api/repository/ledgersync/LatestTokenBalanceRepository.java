a7065f2dd8567dedeb7a9db477618693912b4915
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-05-07 17:52:01 +0700

    chore: update address controller when migrate with new ls schema

--- src/main/java/org/cardanofoundation/explorer/api/repository/ledgersync/LatestTokenBalanceRepository.java
+++ src/main/java/org/cardanofoundation/explorer/api/repository/ledgersync/LatestTokenBalanceRepository.java
@@ -1,5 +1,6 @@
 package org.cardanofoundation.explorer.api.repository.ledgersync;
 
+import org.springframework.data.domain.Page;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
@@ -14,15 +15,51 @@ import org.cardanofoundation.explorer.common.entity.ledgersync.LatestTokenBalanc
 public interface LatestTokenBalanceRepository extends
     JpaRepository<LatestTokenBalance, AddressBalanceId> {
 
+  @Query(value =
+      """ 
+          SELECT ma.fingerprint as fingerprint,
+          ma.policy as policy,
+          ma.name as tokenName,
+          ltb.quantity as quantity,
+          am.url as url, am.decimals as decimals, am.ticker as ticker,
+          am.logo as logo, am.description as description, am.subject as subject
+          FROM LatestTokenBalance ltb
+          INNER JOIN MultiAsset ma ON ma.unit = ltb.unit
+          LEFT JOIN AssetMetadata am ON ma.fingerprint = ma.fingerprint
+          WHERE ltb.address = :address
+          AND ltb.quantity > 0
+          """)
+  Page<AddressTokenProjection> findTokenAndBalanceByAddress(
+      @Param("address") String address, Pageable pageable);
+
+  @Query(value =
+      """ 
+          SELECT ma.fingerprint as fingerprint,
+          ma.policy as policy,
+          ma.name as tokenName,
+          ltb.quantity as quantity,
+          am.url as url, am.decimals as decimals, am.ticker as ticker,
+          am.logo as logo, am.description as description, am.subject as subject
+          FROM LatestTokenBalance ltb
+          INNER JOIN MultiAsset ma ON ma.unit = ltb.unit
+          LEFT JOIN AssetMetadata am ON ma.fingerprint = ma.fingerprint
+          WHERE ltb.address = :address
+          AND (lower(ma.nameView) LIKE CONCAT('%', :searchValue, '%') OR ma.fingerprint = :searchValue)
+          AND ltb.quantity > 0
+          """)
+  Page<AddressTokenProjection> findTokenAndBalanceByAddressAndNameView(
+      @Param("address") String address,
+      @Param("searchValue") String searchValue,
+      Pageable pageable);
+
   @Query(
       value = """
-  
-  SELECT ltb.address as address, ltb.quantity as quantity, ma.nameView as tokenName, ma.fingerprint as fingerprint
-            FROM LatestTokenBalance ltb
-            inner JOIN MultiAsset ma ON ma.unit = ltb.unit
-            WHERE ma.policy = :policy
-  order by ltb.quantity desc
-""")
+      SELECT ltb.address as address, ltb.quantity as quantity, ma.nameView as tokenName, ma.fingerprint as fingerprint
+      FROM LatestTokenBalance ltb
+               inner JOIN MultiAsset ma ON ma.unit = ltb.unit
+      WHERE ma.policy = :policy
+      order by ltb.quantity desc
+      """)
   List<AddressTokenProjection> findAddressAndBalanceByPolicy(
       @Param("policy") String policy, Pageable pageable);
 }
