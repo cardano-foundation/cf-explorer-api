b9864a3ea941aaf26ba9520f1a1e5075ddb90a36
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-06-12 15:55:33 +0700

    feat: update AddressServiceImpl

--- src/main/java/org/cardanofoundation/explorer/api/mapper/AddressMapper.java
+++ src/main/java/org/cardanofoundation/explorer/api/mapper/AddressMapper.java
@@ -1,18 +1,12 @@
 package org.cardanofoundation.explorer.api.mapper;
 
+import org.cardanofoundation.explorer.common.entity.ledgersyncsagg.Address;
 import org.mapstruct.Mapper;
 
-import org.cardanofoundation.explorer.api.model.response.address.AddressFilterResponse;
 import org.cardanofoundation.explorer.api.model.response.address.AddressResponse;
-import org.cardanofoundation.explorer.api.model.response.contract.ContractFilterResponse;
-import org.cardanofoundation.explorer.common.entity.ledgersync.Address;
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
 
-  ContractFilterResponse fromAddressToContractFilter(Address address);
-
   AddressResponse fromAddress(Address address);
-
-  AddressFilterResponse fromAddressToFilterResponse(Address address);
 }
