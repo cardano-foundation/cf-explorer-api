62d08651d3ce118fe59710fc27a53e0f7c963f4a
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-06-12 15:53:18 +0700

    feat: add new ls agg dbsource configuration

---
+++ src/main/java/org/cardanofoundation/explorer/api/repository/ledgersyncagg/AggregateAddressTokenRepository.java
@@ -0,0 +1,16 @@
+package org.cardanofoundation.explorer.api.repository.ledgersyncagg;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.cardanofoundation.explorer.common.entity.ledgersyncsagg.AggregateAddressToken;
+
+
+public interface AggregateAddressTokenRepository
+    extends JpaRepository<AggregateAddressToken, Long> {
+
+  List<AggregateAddressToken> findAllByUnitAndDayBetween(
+      String unit, LocalDate from, LocalDate to);
+}
