9f579d0ce4c6a9d38a2e9e3e4a81329bb4deba2e
Sotatek-DucPhung <duc.phung@sotatek.com>
2024-06-24 11:46:18 +0700

    fix: update drep api

--- src/main/java/org/cardanofoundation/explorer/api/service/impl/DRepServiceImpl.java
+++ src/main/java/org/cardanofoundation/explorer/api/service/impl/DRepServiceImpl.java
@@ -254,6 +254,14 @@ public class DRepServiceImpl implements DRepService {
       dRepFilterRequest.setVotingPowerTo(1.0);
     }
 
+    if (dRepFilterRequest.getMinGovParticipationRate() == null) {
+      dRepFilterRequest.setMinGovParticipationRate(0.0);
+    }
+
+    if (dRepFilterRequest.getMaxGovParticipationRate() == null) {
+      dRepFilterRequest.setMaxGovParticipationRate(1.0);
+    }
+
     Page<DRepFilterResponse> dRepInfoPage =
         drepInfoRepository
             .getDRepInfoByFilterRequest(
