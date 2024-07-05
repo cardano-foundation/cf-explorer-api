ad4c8fb4c54254242153779b78a43c74848ee47e
Kammerlo <thomas.kammerlocher@cardanofoundation.org>
2024-07-05 14:22:44 +0200

    added additional query for testing

--- src/main/java/org/cardanofoundation/explorer/api/repository/ledgersync/AddressBalanceRepository.java
+++ src/main/java/org/cardanofoundation/explorer/api/repository/ledgersync/AddressBalanceRepository.java
@@ -10,12 +10,18 @@ import java.util.List;
 
 public interface AddressBalanceRepository extends JpaRepository<AddressBalance, Long> {
 
-    @Query(value =
-            """
-            SELECT a.address as address, a.quantity as quantity FROM AddressBalance a
-            LEFT OUTER JOIN AddressBalance b
-            ON a.address = b.address AND a.slot < b.slot WHERE b.address IS NULL AND a.unit = 'lovelace'
-            ORDER BY a.quantity DESC LIMIT :count
+//    @Query(value =
+//            """
+//            SELECT a.address as address, a.quantity as quantity FROM AddressBalance a
+//            LEFT OUTER JOIN AddressBalance b
+//            ON a.address = b.address AND a.slot < b.slot WHERE b.address IS NULL AND a.unit = 'lovelace'
+//            ORDER BY a.quantity DESC LIMIT :count
+//            """)
+    @Query(value = """
+            SELECT a.address as address, a.slot, a.quantity as quantity FROM AddressBalance a
+            WHERE a.slot = (SELECT max(slot) FROM AddressBalance b WHERE b.address = a.address)
+            AND unit = 'lovelace'
+            ORDER BY quantity DESC LIMIT :count
             """)
     List<AddressQuantityProjection> findTopAddresses(@Param("count") int count);
 
