e82f570267f428879f9c34be64573fac2707b8cc
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2024-09-03 16:10:56 +0100

    chore: touch ups

--- src/main/java/org/cardanofoundation/explorer/api/controller/SupplyController.java
+++ src/main/java/org/cardanofoundation/explorer/api/controller/SupplyController.java
@@ -1,6 +1,5 @@
 package org.cardanofoundation.explorer.api.controller;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-import org.cardanofoundation.conversions.CardanoConverters;
 import org.cardanofoundation.explorer.api.common.constant.CommonConstant;
 import org.cardanofoundation.explorer.api.config.LogMessage;
 import org.cardanofoundation.explorer.api.repository.ledgersync.AdaPotsRepository;
@@ -25,12 +23,7 @@ import org.cardanofoundation.explorer.common.entity.ledgersync.AdaPots;
 @Tag(name = "supply", description = "ada token supply information")
 public class SupplyController {
 
-  //  ➜  ~ curl -H 'Host: explorer.cardano.org' --insecure https://147.75.85.167/supply/circulating
-  // 35969368510.862%
-  // ➜  ~ curl -H 'Host: explorer.cardano.org' --insecure https://147.75.85.167/supply/total
-  // 37170426657.961%
-
-  private final CardanoConverters cardanoConverters;
+  private static final BigInteger ONE_ADA_IN_LOVELACES = BigInteger.valueOf(1_000_000L);
 
   private final AdaPotsRepository adaPotsRepository;
 
@@ -47,7 +40,7 @@ public class SupplyController {
             .toBigInteger()
             .subtract(latestAdaPots.getReserves())
             .subtract(latestAdaPots.getTreasury());
-    return ResponseEntity.ok(circulatingSupply.divide(BigInteger.valueOf(1_000_000L)).longValue());
+    return ResponseEntity.ok(circulatingSupply.divide(ONE_ADA_IN_LOVELACES).longValue());
   }
 
   @GetMapping("/total")
@@ -59,6 +52,6 @@ public class SupplyController {
   public ResponseEntity<Long> getSupplyTotal() {
     AdaPots latestAdaPots = adaPotsRepository.findFirstByOrderByEpochNoDescSlotNoDesc();
     var supplyTotal = CommonConstant.TOTAL_ADA.toBigInteger().subtract(latestAdaPots.getReserves());
-    return ResponseEntity.ok(supplyTotal.divide(BigInteger.valueOf(1_000_000L)).longValue());
+    return ResponseEntity.ok(supplyTotal.divide(ONE_ADA_IN_LOVELACES).longValue());
   }
 }
