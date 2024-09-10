4f746a7b5a644b83370a7aeec2f3220f1c45c290
Sotatek-ThinhVu <thinh.vu@sotatek.com>
2024-09-10 09:53:54 +0700

    feat: calculate carbon emissions of an address or stake address

---
+++ src/main/java/org/cardanofoundation/explorer/api/service/MiCARService.java
@@ -0,0 +1,7 @@
+package org.cardanofoundation.explorer.api.service;
+
+import org.cardanofoundation.explorer.api.model.response.micar.AddressCarbonEmissionResponse;
+
+public interface MiCARService {
+  AddressCarbonEmissionResponse getCarbonEmissionsByAddressAndPool(String address);
+}
